dc4213533fa38e5fe2f44cfb5e9285a81115a887
Tanner Cook <tannercook336@gmail.com>
2020-10-29 18:55:37 -0400

    cleanup

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,6 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
 self.addEventListener("install", function(evt) {
 evt.waitUntil(
   caches.open(CACHE_NAME).then(cache => {
@@ -16,7 +15,6 @@ evt.waitUntil(
 self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", function(evt) {
 evt.waitUntil(
   caches.keys().then(keyList => {
@@ -34,27 +32,26 @@ evt.waitUntil(
 self.clients.claim();
 });
 
-// fetch
 self.addEventListener("fetch", evt => {
   if(evt.request.url.includes('/api/')) {
       console.log('[Service Worker] Fetch(data)', evt.request.url);
   
-evt.respondWith(
-              caches.open(DATA_CACHE_NAME).then(cache => {
-              return fetch(evt.request)
-              .then(response => {
-                  if (response.status === 200){
-                      cache.put(evt.request.url, response.clone());
-                  }
-                  return response;
-              })
-              .catch(err => {
-                  return cache.match(evt.request);
-              });
-          })
-          );
-          return;
-      }
+  evt.respondWith(
+    caches.open(DATA_CACHE_NAME).then(cache => {
+      return fetch(evt.request)
+        .then(response => {
+          if (response.status === 200){
+          cache.put(evt.request.url, response.clone());
+          }
+        return response;
+        })
+        .catch(err => {
+            return cache.match(evt.request);
+        });
+    })
+  );
+  return;
+  }
 
 evt.respondWith(
   caches.open(CACHE_NAME).then( cache => {
